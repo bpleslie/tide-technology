a86cdf79b40f45e7a72e99be001f03ae0679dabc
bpleslie <bradleypleslie@gmail.com>
2018-07-21 08:32:48 -0700

    register service worker

---
+++ sw.js
@@ -0,0 +1,14 @@
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('tide-technology').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/assets/css/main.css'
+     ]);
+   })
+ );
+});
